e470536ffa06cf54db8380649ee9c941967f3549
Todd Meadows <todd@meadowsnet.net>
2012-06-02 22:48:45 -0500

    Initial checkin

---
+++ Adherent/DataObjects/Settings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdherentSampleOven.DataObjects
+{
+    public enum TemperatureFormatEnum { Celsius, Farenheit };
+
+    public class Settings
+    {
+        public IDictionary<byte, MccPortInformation> SampleConfigurationDictionary
+        {
+            get;
+            set;
+        }
+
+        public int TempBoardNumber
+        {
+            get;
+            set;
+        }
+        
+        public byte TempPortNumber
+        {
+            get;
+            set;
+        }
+
+        public int DIOBoardNumber
+        {
+            get;
+            set;
+        }
+
+        public TemperatureFormatEnum TemperatureFormat
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
